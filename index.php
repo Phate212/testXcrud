6875425ecd28767f3fef353fbf371997d1acd50b
Raymond Seger <raymondseger@live.com>
2016-03-15 13:58:44 +0700

    test simple one

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+include('./xcrud/xcrud.php');
+$xcrud = Xcrud::get_instance();
+$xcrud->table('base_fields');
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Some page title</title>
+</head>
+ 
+<body>
+ 
+<?php
+    echo $xcrud->render();
+?>
+ 
+</body>
+</html>
